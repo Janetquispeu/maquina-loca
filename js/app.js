5f7852370fee95bf2a30fabef49c85709665e038
janetquispeu@gmail.com <janetquispeu@gmail.com>
2016-09-15 18:08:25 -0500

    subiendo

---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+	document.getElementById('cri').addEventListener("click", function(){
+ 	var n=document.getElementById("nom-color").value;
+	 n.setAttribute(div:hidden, "red");
+ 	});    
+
+		
+});
